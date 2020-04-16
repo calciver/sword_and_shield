575b4a7f20cb6aef7bfd07205dfff8bb49de70d0
unknown <russchua@gmail.com>
2020-04-17 00:05:12 +0800

    Adding a rarity and HA parser

--- read_serebii/read_website.py
+++ read_serebii/read_website.py
@@ -2,7 +2,8 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 # Fetch the html file
-response = urlopen('https://www.serebii.net/swordshield/maxraidbattles/den1.shtml')
+# response = urlopen('https://www.serebii.net/swordshield/maxraidbattles/den1.shtml')
+response = urlopen('https://www.serebii.net/swordshield/maxraidbattles/den85.shtml')
 html_doc = response.read()
 
 # Parse the html file
@@ -15,17 +16,54 @@ def find_pokemon_id(name_box):
     pkmn_ids = []
     for item in pokemon_entry:
         try:
-            # print(item.a)
             basename = item.a.img['src'].split('/')[-1]
             pkmn_id = basename.split('.png')[0]
-            pkmn_ids.append(pkmn_id)
-            # print(item.a.img['src'])
-            # print(pkmn_id)
-            # i += 1
-            
+            pkmn_ids.append(pkmn_id)            
         except:
             pass
     return(pkmn_ids)
+
+def find_pokemon_rarity_ability(name_box):
+    total_len = len(name_box.find_all('tr',recursive=False))
+    rarity_list = []
+    ability_list = []
+    i = 4
+    while i<total_len:
+        rarity_entry = name_box.find_all('tr',recursive=False)[i]
+        parse_rarity(rarity_entry)
+        rarity_list.append(rarity_entry)
+
+        ability_entry = name_box.find_all('tr',recursive=False)[i+1]
+        ability_list.append(ability_entry)
+        i += 6
+
+    # print(rarity_list)
+    # print(ability_list)
+    # print(len(rarity_list),len(ability_list))
+
+def parse_rarity(rarity_entry):
+    all_rarities = rarity_entry.find_all('td')
+    for each_rarity_info in all_rarities:
+        print(each_rarity_info)
+        for b in each_rarity_info.find_all('b'):
+            if b.next_sibling:
+                print(b.next_sibling)
+        for br in each_rarity_info.find_all('br'):
+            if br.next_sibling:
+                print(br.next_sibling)
+        # print(each_rarity_info)
+
+find_pokemon_rarity_ability(name_box)
+
+
+# print(len(name_box.find_all('tr',recursive=False)))
+# print(name_box.find_all('tr',recursive=False)[4])
+# print(name_box.find_all('tr',recursive=False)[5])
+# print(name_box.find_all('tr',recursive=False)[4+6])
+# print(name_box.find_all('tr',recursive=False)[5+6])
+
+# print(name_box.find_all('tr',recursive=False)[4+6+6])
+# print(name_box.find_all('tr',recursive=False)[5+6+6])
 
 # print(find_pokemon_id(name_box))
 
